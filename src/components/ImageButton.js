2a8f4d7eb3923619798de4abe06d771d24debf1d
diojer <diogodinosaurus@gmail.com>
2023-09-18 15:03:34 +0100

    minor changes, fixed imagebuttons and some styling

--- src/components/ImageButton.js
+++ src/components/ImageButton.js
@@ -7,16 +7,16 @@ const SHAPES = ["imgb--rect", "imgb--square", "imgb--thin"];
 export const ImageButton = ({ children, image, shape, color, path }) => {
   const checkShape = SHAPES.includes(shape) ? shape : SHAPES[0];
   return (
-    <div
-      className={`image-button ${checkShape}`}
-      style={{
-        backgroundImage: `url(${image})`,
-        backgroundColor: `${color}`,
-      }}
-    >
-      <Link rel="stylesheet" to={path} className="image-button-link">
+    <Link to={path} className={`image-button-link ${checkShape}`}>
+      <div
+        className={`image-button ${checkShape}`}
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundColor: `${color}`,
+        }}
+      >
         <p className="image-button-text">{children}</p>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
